cb05fc3469ea543865490ab2980ec097d6b2d321
Martin Vavrusak <vavmar@gmail.com>
2013-11-14 07:46:13 +0100

    beg

---
+++ src/cz/muni/fi/vavmar/experimental/base/MyActivator.java
@@ -0,0 +1,66 @@
+package cz.muni.fi.vavmar.experimental.base;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.adempiere.plugin.utils.AdempiereActivator;
+import org.compiere.util.CLogger;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
+
+public class MyActivator extends AdempiereActivator {
+
+	CLogger logger;
+	
+	MyActivator (){
+		logger = CLogger.getCLogger(MyActivator.class);
+	}
+	
+	@Override
+	public String getName() {
+		System.out.println("xXxXxXxXxXxXxXxXxXxXxXxX getName xXxXxXxXxXxXxXxXxXxXxXxXxXxXxX");
+		logger.log(Level.SEVERE, "xXxXxXxXxXxXxXxXxXxXxXxX Logging: getName xXxXxXxXxXxXxXxXxXxXxXxXxXxXxX");
+		throw new RuntimeException("Metoda getName");
+		//return "Experimental plugin";
+	}
+
+	@Override
+	public String getVersion() {
+		System.out.println("xXxXxXxXxXxXxXxXxXxXxXxX getVersion xXxXxXxXxXxXxXxXxXxXxXxXxXxXxX");
+		logger.log(Level.SEVERE, "xXxXxXxXxXxXxXxXxXxXxXxX Logging: getVersion xXxXxXxXxXxXxXxXxXxXxXxXxXxXxX");
+		throw new RuntimeException("Metoda getVersion");
+		//return "return 0.1";
+	}
+
+	@Override
+	public String getDescription() {
+		System.out.println("xXxXxXxXxXxXxXxXxXxXxXxX getDescription xXxXxXxXxXxXxXxXxXxXxXxXxXxXxX");
+		logger.log(Level.SEVERE, "xXxXxXxXxXxXxXxXxXxXxXxX Logging: getDescription xXxXxXxXxXxXxXxXxXxXxXxXxXxXxX");
+		throw new RuntimeException("Metoda getDescription");
+		//return "Plugin for experiments with iDempiere";
+	}
+
+	@Override
+	protected void install() {
+		super.install();
+		System.out.println("xXxXxXxXxXxXxXxXxXxXxXxX Install xXxXxXxXxXxXxXxXxXxXxXxXxXxXxX");
+		logger.log(Level.SEVERE, "xXxXxXxXxXxXxXxXxXxXxXxX Logging: Install xXxXxXxXxXxXxXxXxXxXxXxXxXxXxX");
+		throw new RuntimeException("Metoda install");
+	}
+
+	@Override
+	protected void start() {
+		super.start();
+		System.out.println("xXxXxXxXxXxXxXxXxXxXxXxX Start xXxXxXxXxXxXxXxXxXxXxXxXxXxXxX");
+		logger.log(Level.SEVERE, "xXxXxXxXxXxXxXxXxXxXxXxX Logging: Start xXxXxXxXxXxXxXxXxXxXxXxXxXxXxX");
+		throw new RuntimeException("Metoda start");
+	}
+
+	@Override
+	protected void stop() {
+		super.stop();
+		System.out.println("xXxXxXxXxXxXxXxXxXxXxXxX Stop xXxXxXxXxXxXxXxXxXxXxXxXxXxXxX");
+		logger.log(Level.SEVERE, "xXxXxXxXxXxXxXxXxXxXxXxX Logging: Stop xXxXxXxXxXxXxXxXxXxXxXxXxXxXxX");
+		throw new RuntimeException("Metoda stop");
+	}
+
+}
