44a90594d1640a3c15b9cf6998133c01710784bd
Martin Vavrusak <vavmar@gmail.com>
2014-01-23 16:52:12 +0100

    Maven Editor project founded

---
+++ Editor/src/main/java/cz/fi/muni/vavmar/editor/App.java
@@ -0,0 +1,15 @@
+package cz.fi.muni.vavmar.editor;
+
+import org.netbeans.api.visual.widget.Widget;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
