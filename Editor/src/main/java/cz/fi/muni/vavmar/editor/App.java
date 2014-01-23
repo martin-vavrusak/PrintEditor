50a587b107525acdf6a7b57781655cdbd32c5031
Martin Vavrusak <vavmar@gmail.com>
2014-01-23 17:55:34 +0100

    Basic screen setup

--- Editor/src/main/java/cz/fi/muni/vavmar/editor/App.java
+++ Editor/src/main/java/cz/fi/muni/vavmar/editor/App.java
@@ -1,5 +1,10 @@
 package cz.fi.muni.vavmar.editor;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import org.netbeans.api.visual.widget.Scene;
 import org.netbeans.api.visual.widget.Widget;
 
 /**
@@ -10,6 +15,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Editor." );
+        Scene scene = new Scene();
+        
+        //Create new base canvas and asociate JScrollPane
+        JScrollPane canvas = new JScrollPane ( scene.createView() );
+        canvas.getHorizontalScrollBar ().setUnitIncrement (32);
+        canvas.getHorizontalScrollBar ().setBlockIncrement (256);
+        canvas.getVerticalScrollBar ().setUnitIncrement (32);
+        canvas.getVerticalScrollBar ().setBlockIncrement (256);
+        canvas.setBackground(Color.BLUE);
+        
+        //Create basic frame and add previously created canvas
+        int width=800,height=600;
+        JFrame frame = new JFrame ();//new JDialog (), true);
+        frame.add (canvas, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation (JFrame.DISPOSE_ON_CLOSE);
+        
+        //Set maximal size of frame
+        java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setBounds((screenSize.width-width)/2, (screenSize.height-height)/2, width, height);   //(screenSize.width-width)/2 "Sets position" to the middle of screen
+        frame.setVisible (true);
     }
 }
