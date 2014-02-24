5eb509c5c3e2ff93142c10b7d42e1b6de3bf753c
Martin Vavrusak <vavmar@gmail.com>
2014-02-24 22:22:17 +0100

    Priprava na editaci textu - implemntovano zobrazeni popup menu a reakce na dvojklik

--- Editor/src/main/java/cz/fi/muni/vavmar/editor/tools/TextTool.java
+++ Editor/src/main/java/cz/fi/muni/vavmar/editor/tools/TextTool.java
@@ -6,9 +6,17 @@
 
 package cz.fi.muni.vavmar.editor.tools;
 
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.Icon;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.netbeans.api.visual.action.ActionFactory;
+import org.netbeans.api.visual.action.EditProvider;
+import org.netbeans.api.visual.action.PopupMenuProvider;
 import org.netbeans.api.visual.widget.LabelWidget;
 import org.netbeans.api.visual.widget.Scene;
 import org.netbeans.api.visual.widget.Widget;
@@ -40,8 +48,69 @@ public class TextTool extends AbstractTool {
         logger.trace("TextTool: vytvarim widget");
         LabelWidget lw = new LabelWidget(scene, "TextTool");
 //        lw.getActions().addAction(ActionFactory.createMoveAction());
+        lw.getActions().addAction(ActionFactory.createEditAction( new EditTextProvider() ));
+        lw.getActions().addAction(ActionFactory.createPopupMenuAction( new TextPopupMenuProvider() ));
         return lw;
     }
     
+    /**
+     * Shows dialog for fillin parameters of text and set all parameters to widget
+     * @param widget - widget which parameters should be changed
+     */
+    protected void showEditDialog(Widget widget){
+        logger.trace("Zobrazuji dialogove okno!");
+    }
 
+    
+    /**
+     * Reaguje na dvojklik levym mysitkem a deleguje dobrazeni dialogoveho okna {@link #showEditDialog()} pro nastaveni parametru textu
+     */
+    private class EditTextProvider implements EditProvider {
+        private final Logger logger = LogManager.getLogger(EditTextProvider.class);
+        
+        public void edit(Widget widget) {
+            logger.trace("Edit firing");
+            showEditDialog(widget);
+        }
+        
+    }
+    
+    /**
+     * Zobrazi menu pri kliknuti pravym tlacitkem mysi na widget a pri zvoleni polozky
+     * nastaveni vlastnosti textu deleguje zobrazeni dialogoveho okna a nastaveni na
+     * {@link #showEditDialog()} materske tridy
+     */
+    private class TextPopupMenuProvider implements PopupMenuProvider, ActionListener {
+        private final Logger logger = LogManager.getLogger(TextPopupMenuProvider.class);
+
+        private final JPopupMenu menu;
+        private final String PROPERTIES = "Properties";
+        private Widget ownerWidget;
+        
+        public TextPopupMenuProvider() {
+            menu = new JPopupMenu("Menu");
+            JMenuItem menuItem;
+            
+            menuItem = new JMenuItem(PROPERTIES);
+            menuItem.setActionCommand(PROPERTIES);
+            menuItem.addActionListener(this);
+            menu.add(menuItem);
+        }
+        
+        
+        public JPopupMenu getPopupMenu(Widget widget, Point localLocation) {
+            logger.trace("Owner Widget:" + widget);
+            ownerWidget = widget;           //Nastavi widget na kterem bylo vyvolano menu pro dalsi zpracovani v "actionPerformed()"
+            return menu;
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            logger.trace("");
+            if ( PROPERTIES.equals(e.getActionCommand()) ){
+                logger.trace("Zobrazit menu!");
+                showEditDialog(ownerWidget);        //deleguje zobrazeni dialogoveho okna a nastaveni hodnot na metodu materske tridy
+            }
+        }
+        
+    }
 }
