0cb7f8cd98482365cde4ddfda019c7784dfdbb7d
Martin Vavrušák <vavmar@gmail.com>
2014-04-11 16:47:59 +0200

    funkcni logovani pomoci Log4J

--- src/cz/muni/fi/vavmar/printeditor/Activator.java
+++ src/cz/muni/fi/vavmar/printeditor/Activator.java
@@ -2,13 +2,16 @@ package cz.muni.fi.vavmar.printeditor;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+//import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.LogManager;
 
 public class Activator implements BundleActivator {
 
 	private static BundleContext context;
-
+//	private static Logger logger = LogManager.getLogger(Activator.class);
 	static BundleContext getContext() {
 		return context;
+		
 	}
 
 	/*
@@ -16,7 +19,18 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
+		System.out.println("Bundle activated.");
 		Activator.context = bundleContext;
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			
+			
+			@Override
+			public void run() {
+				MyClass myClass = new MyClass();
+				myClass.startProgram();
+			}
+		});
+		System.out.println("Program started.");
 	}
 
 	/*
@@ -24,6 +38,7 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
+		System.out.println("Bundle stopped.");
 		Activator.context = null;
 	}
 
